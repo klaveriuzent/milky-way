b70d947443b383cd0b60fdf15ba43c57ab53f5f6
klaveriuzent <jefrychiedi1999@gmail.com>
2024-01-26 17:14:58 +0700

    init antd theme

--- src/stories/button/Button.tsx
+++ src/stories/button/Button.tsx
@@ -1,20 +1,33 @@
 import './button.scss';
 import { ButtonProps } from './Button.interface';
-import { Button as AntdButton, ButtonProps as AntdButtonProps } from 'antd';
-
+import { Button as AntdButton, ButtonProps as AntdButtonProps, ConfigProvider } from 'antd';
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({
+  disabled,
   label,
   ...props
 }: ButtonProps) => {
   return (
-    <AntdButton className='storybook-button'
-      {...props as AntdButtonProps}
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: '#00b96b',
+          borderRadius: 2,
+
+          // Alias Token
+          colorBgContainer: '#f6ffed',
+        },
+      }}
     >
-      {label}
-    </AntdButton>
+      <AntdButton className='storybook-button'
+        {...props as AntdButtonProps}
+      >
+        {label}
+      </AntdButton>
+    </ConfigProvider>
   );
 };

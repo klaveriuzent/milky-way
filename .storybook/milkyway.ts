7d7816a4938400a62affeb0b86f577d3e6c4829c
klaveriuzent <jefrychiedi1999@gmail.com>
2024-01-30 14:57:37 +0700

    sample

--- .storybook/milkyway.ts
+++ .storybook/milkyway.ts
@@ -1,11 +1,19 @@
 import { create } from '@storybook/theming/create';
 
 export default create({
-    base: 'dark',
+    base: 'light',
     brandTitle: 'MilkyWay Storybook',
     brandUrl: 'https://storybook.js.org/images/placeholders/350x150.png',
     brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
     brandTarget: '_self',
+
+    // Typography
+    fontBase: '"Roboto", sans-serif',
+    fontCode: 'Orbitron, monospace',
+
+    colorPrimary: '#3A10E5',
+    colorSecondary: '#585C6D',
+
 });
 
 // Refrence

713144887af878cc8aba4d4e559475251d0ba3fa
klaveriuzent <jefrychiedi1999@gmail.com>
2024-01-30 11:12:08 +0700

    preview change light & dark

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,12 @@
 import type { Preview } from "@storybook/react";
 
+const logoSvg = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50">
+    <circle cx="25" cy="25" r="20" fill="#00b96b" />
+    <!-- Tambahkan elemen SVG sesuai kebutuhan -->
+  </svg>
+`;
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,6 +15,12 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      values: [
+        { name: 'light', value: '#fff' },
+        { name: 'dark', value: '#333' },
+      ],
     },
   },
 };
